9559ed48ac09ed4c35e41e60a4a23cd8b2f59bd5
L20L021902 <L20L021902@stu.hit.edu.cn>
2024-03-21 01:17:32 +0800

    finish connection manager

--- src/connection_manager.rs
+++ src/connection_manager.rs
@@ -1,12 +1,12 @@
-use log::debug;
+use log::{debug, warn};
 use async_native_tls::{TlsConnector, Certificate};
 use async_std::{
     io::{BufReader, BufWriter},
     net::{TcpStream, ToSocketAddrs},
     prelude::*,
 };
-use futures::{select, FutureExt, AsyncBufRead, SinkExt, AsyncReadExt, AsyncWriteExt};
-use crate::constants::{Result, Sender, Receiver, ServerEvent, ClientEvent, SERVER_NAME, PROTOCOL_HEADER, ServerToClientMessageType};
+use futures::{select, FutureExt, SinkExt, AsyncReadExt, AsyncWriteExt};
+use crate::constants::{Result, Sender, Receiver, ServerEvent, ClientEvent, SERVER_NAME, PROTOCOL_HEADER, ServerToClientMessageTypePrimitive, ConferenceJoinSalt, ConferenceEncryptionSalt};
 
 async fn start_connection_manager(
     server_address: impl ToSocketAddrs,
@@ -57,7 +57,7 @@ async fn handle_handshake(reader: &mut (impl AsyncReadExt + Unpin), writer: &mut
     writer.flush().await?;
 
     reader.read_exact(&mut server_response).await?;
-    if server_response[0] != ServerToClientMessageType::HandshakeAcknowledged as u8 {
+    if server_response[0] != ServerToClientMessageTypePrimitive::HandshakeAcknowledged as u8 {
         return Err("Handshake failed".into());
     }
 
@@ -66,20 +66,156 @@ async fn handle_handshake(reader: &mut (impl AsyncReadExt + Unpin), writer: &mut
     Ok(())
 }
 
-async fn read_server_event(event_type: u8, reader: &mut (impl AsyncBufRead + Unpin)) -> Result<ServerEvent> {
-    todo!();
+async fn read_server_event(event_type: u8, reader: &mut (impl AsyncReadExt + Unpin)) -> Result<ServerEvent> {
+    if let Ok(server_event_type) = ServerToClientMessageTypePrimitive::try_from(event_type) {
+        match server_event_type {
+            ServerToClientMessageTypePrimitive::HandshakeAcknowledged => {
+                warn!("Server sent unexpected handshake acknowledgement");
+                Ok(ServerEvent::HandshakeAcknowledged)
+            },
+            ServerToClientMessageTypePrimitive::ConferenceCreated => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::ConferenceCreated((nonce, conference_id)))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceJoinSalt => {
+                let mut buffer_small: [u8; 4] = [0; 4];
+                let mut join_salt: ConferenceJoinSalt = [0; 32];
+                reader.read_exact(&mut buffer_small).await?;
+                let nonce = u32::from_be_bytes(buffer_small);
+                reader.read_exact(&mut buffer_small).await?;
+                let conference_id = u32::from_be_bytes(buffer_small);
+                reader.read_exact(&mut join_salt).await?;
+                Ok(ServerEvent::ConferenceJoinSalt((nonce, conference_id, join_salt)))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceJoined => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let number_of_peers = u32::from_be_bytes(buffer);
+                let mut buffer_large: ConferenceEncryptionSalt = [0; 32];
+                reader.read_exact(&mut buffer_large).await?;
+                Ok(ServerEvent::ConferenceJoined((nonce, conference_id, number_of_peers, buffer_large)))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceLeft => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::ConferenceLeft((nonce, conference_id)))
+            },
+            ServerToClientMessageTypePrimitive::MessageAccepted => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::MessageAccepted((nonce, conference_id)))
+            },
+            ServerToClientMessageTypePrimitive::IncomingMessage => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let message_length = u32::from_be_bytes(buffer);
+                let mut message = Vec::with_capacity(message_length as usize);
+                reader.read_exact(&mut message).await?;
+                Ok(ServerEvent::IncomingMessage((conference_id, message)))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceRestructuring => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let number_of_peers = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::ConferenceRestructuring((conference_id, number_of_peers)))
+            },
+            ServerToClientMessageTypePrimitive::GeneralError => {
+                Ok(ServerEvent::GeneralError)
+            },
+            ServerToClientMessageTypePrimitive::ConferenceCreationError => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::ConferenceCreationError(nonce))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceJoinSaltError => {
+                let mut buffer_small: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer_small).await?;
+                let nonce = u32::from_be_bytes(buffer_small);
+                reader.read_exact(&mut buffer_small).await?;
+                let conference_id = u32::from_be_bytes(buffer_small);
+                Ok(ServerEvent::ConferenceJoinSaltError((nonce, conference_id)))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceJoinError => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::ConferenceJoinError((nonce, conference_id)))
+            },
+            ServerToClientMessageTypePrimitive::ConferenceLeaveError => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::ConferenceLeaveError((nonce, conference_id)))
+            },
+            ServerToClientMessageTypePrimitive::MessageError => {
+                let mut buffer: [u8; 4] = [0; 4];
+                reader.read_exact(&mut buffer).await?;
+                let nonce = u32::from_be_bytes(buffer);
+                reader.read_exact(&mut buffer).await?;
+                let conference_id = u32::from_be_bytes(buffer);
+                Ok(ServerEvent::MessageError((nonce, conference_id)))
+            },
+        }
+    } else {
+        Err("Invalid server event type".into())
+    }
 }
 
 /// Write a client event to the server, returning whether the connection should be kept open
 async fn write_client_event(event: ClientEvent, writer: &mut (impl AsyncWriteExt + Unpin)) -> Result<bool> {
+    writer.write_all(&[event.value()]).await?;
     match event {
+        ClientEvent::CreateConference((nonce, password_hash, join_salt, encryption_salt)) => {
+            writer.write_all(&nonce.to_be_bytes()).await?;
+            writer.write_all(&password_hash).await?;
+            writer.write_all(&join_salt).await?;
+            writer.write_all(&encryption_salt).await?;
+        },
+        ClientEvent::GetConferenceJoinSalt((nonce, conference_id)) => {
+            writer.write_all(&nonce.to_be_bytes()).await?;
+            writer.write_all(&conference_id.to_be_bytes()).await?;
+        },
+        ClientEvent::JoinConference((nonce, conference_id, password_hash)) => {
+            writer.write_all(&nonce.to_be_bytes()).await?;
+            writer.write_all(&conference_id.to_be_bytes()).await?;
+            writer.write_all(&password_hash).await?;
+        },
+        ClientEvent::LeaveConference((nonce, conference_id)) => {
+            writer.write_all(&nonce.to_be_bytes()).await?;
+            writer.write_all(&conference_id.to_be_bytes()).await?;
+        },
+        ClientEvent::SendMessage((nonce, message)) => {
+            writer.write_all(&nonce.to_be_bytes()).await?;
+            writer.write_all(&message.conference.to_be_bytes()).await?;
+            writer.write_all(&message.message.len().to_be_bytes()).await?;
+            writer.write_all(&message.message).await?;
+        },
         ClientEvent::Disconnect => {
-            writer.write_all(&[ClientEvent::Disconnect as u8]).await?;
             writer.flush().await?;
             return Ok(false);
-        },
-        _ => {
-            todo!();
         },
     }
 
--- src/constants.rs
+++ src/constants.rs
@@ -5,11 +5,105 @@ pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + S
 pub type Sender<T> = mpsc::UnboundedSender<T>;
 pub type Receiver<T> = mpsc::UnboundedReceiver<T>;
 
+pub type ConferenceId = u32;
+pub type NumberOfPeers = u32;
+pub type EncryptionKey = [u8; 32];
+pub type PacketNonce = u32;
+pub type MessageLength = u32;
+pub type PasswordHash = [u8; 32];
+pub type ConferenceJoinSalt = [u8; 32];
+pub type ConferenceEncryptionSalt = [u8; 32];
 
-pub enum ServerEvent {
 
+pub struct Message {
+    pub conference: ConferenceId,
+    pub message: Vec<u8>,
 }
 
+#[repr(u8)]
 pub enum ClientEvent {
+    CreateConference((PacketNonce, PasswordHash, ConferenceJoinSalt, ConferenceEncryptionSalt)) = 0x01,
+    GetConferenceJoinSalt((PacketNonce, ConferenceId)) = 0x02,
+    JoinConference((PacketNonce, ConferenceId, PasswordHash)) = 0x03,
+    LeaveConference((PacketNonce, ConferenceId)) = 0x04,
+    SendMessage((PacketNonce, Message)) = 0x05,
+    Disconnect = 0x06,
+}
+
+impl ClientEvent {
+    pub fn value(&self) -> u8 {
+        unsafe { *(self as *const Self as *const u8) }
+    }
+}
+
+#[repr(u8)]
+pub enum ServerEvent {
+    HandshakeAcknowledged = 0x00,
+    ConferenceCreated((PacketNonce, ConferenceId)) = 0x01,
+    ConferenceJoinSalt((PacketNonce, ConferenceId, ConferenceJoinSalt)) = 0x02,
+    ConferenceJoined((PacketNonce, ConferenceId, NumberOfPeers, ConferenceEncryptionSalt)) = 0x03,
+    ConferenceLeft((PacketNonce, ConferenceId)) = 0x04,
+    MessageAccepted((PacketNonce, ConferenceId)) = 0x05,
+    ConferenceRestructuring((ConferenceId, NumberOfPeers)) = 0x06,
+    IncomingMessage((ConferenceId, Vec<u8>)) = 0x07,
+
+    GeneralError = 0x10,
+    ConferenceCreationError(PacketNonce) = 0x11,
+    ConferenceJoinSaltError((PacketNonce, ConferenceId)) = 0x12,
+    ConferenceJoinError((PacketNonce, ConferenceId)) = 0x13,
+    ConferenceLeaveError((PacketNonce, ConferenceId)) = 0x14,
+    MessageError((PacketNonce, ConferenceId)) = 0x15,
+}
+
+#[repr(u8)]
+pub enum ServerToClientMessageTypePrimitive {
+    HandshakeAcknowledged = 0x00,
+    ConferenceCreated = 0x01,
+    ConferenceJoinSalt = 0x02,
+    ConferenceJoined = 0x03,
+    ConferenceLeft = 0x04,
+    MessageAccepted = 0x05,
+    ConferenceRestructuring = 0x06,
+    IncomingMessage = 0x07,
+
+    GeneralError = 0x10,
+    ConferenceCreationError = 0x11,
+    ConferenceJoinSaltError = 0x12,
+    ConferenceJoinError = 0x13,
+    ConferenceLeaveError = 0x14,
+    MessageError = 0x15,
+}
+
+impl TryFrom<u8> for ServerToClientMessageTypePrimitive {
+    type Error = ();
+
+    fn try_from(v: u8) -> std::result::Result<Self, Self::Error> {
+        match v {
+            x if x == ServerToClientMessageTypePrimitive::HandshakeAcknowledged as u8 => Ok(ServerToClientMessageTypePrimitive::HandshakeAcknowledged),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceCreated as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceCreated),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceJoinSalt as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceJoinSalt),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceJoined as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceJoined),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceLeft as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceLeft),
+            x if x == ServerToClientMessageTypePrimitive::MessageAccepted as u8 => Ok(ServerToClientMessageTypePrimitive::MessageAccepted),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceRestructuring as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceRestructuring),
+            x if x == ServerToClientMessageTypePrimitive::IncomingMessage as u8 => Ok(ServerToClientMessageTypePrimitive::IncomingMessage),
+
+            x if x == ServerToClientMessageTypePrimitive::GeneralError as u8 => Ok(ServerToClientMessageTypePrimitive::GeneralError),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceCreationError as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceCreationError),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceJoinSaltError as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceJoinSaltError),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceJoinError as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceJoinError),
+            x if x == ServerToClientMessageTypePrimitive::ConferenceLeaveError as u8 => Ok(ServerToClientMessageTypePrimitive::ConferenceLeaveError),
+            x if x == ServerToClientMessageTypePrimitive::MessageError as u8 => Ok(ServerToClientMessageTypePrimitive::MessageError),
+            _ => Err(()),
+        }
+    }
+}
+
+pub enum UIEvent {
 
 }
+
+pub const SERVER_NAME: &str = "anonymous-conference.program";
+
+pub const PROTOCOL_HEADER: &[u8] = b"\x1CAnonymousConference protocol";
+
